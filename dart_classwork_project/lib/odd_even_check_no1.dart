64e83a8db0f50671d3b527a871a1497ffac08f30
Samip-Subedi <samipsubedi68@gmail.com>
2024-11-18 22:44:38 +0545

    number 1 complete

---
+++ dart_classwork_project/lib/odd_even_check_no1.dart
@@ -0,0 +1,11 @@
+void checkOddOrEven(int number) {
+  if (number % 2 == 0) {
+    print("$number is Even");
+  } else {
+    print("$number is Odd");
+  }
+}
+
+void main() {
+  checkOddOrEven(7); 
+}
